b81bdccc5bd99d6c3dd961b9396d95b685c51404
Somnath Mandhare <somnathmandhare5@gmail.com>
2025-09-05 19:32:05 +0530

    Initial Commit

---
+++ MatchMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchMate
+//
+//  Created by Somnath Mandhare on 05/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Somnath Mandhare on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
